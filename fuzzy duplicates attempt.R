82d73fcc71d0b5d1f4eab067e9588f932ad5a964
richardvogg <46679869+richardvogg@users.noreply.github.com>
2020-08-26 15:06:51 -0400

    Created function to remove duplicates from a vector with words

--- fuzzy duplicates attempt.R
+++ fuzzy duplicates attempt.R
@@ -7,15 +7,12 @@ library(tictoc)
 ### Data
 
 #Mini Example
-test <- tibble(txt=c("hello","hallo","telefon","telephone"),best_fit=NA,similarity=1)
+test <- c("hello","hallo","telefon","hello","telephone","telephone")
 
 #Blog Texts
 test <- read.table("C:/Richard/R and Python/Datasets/blog.txt",sep="\n") %>%
   unnest_tokens(word,V1) %>% 
-  select(txt=word) %>% 
-  tibble() %>% 
-  count(txt,sort=T) %>% 
-  mutate(best_fit=NA,similarity=1)
+  .$word
 
 #Tweets
 test <- search_tweets("#TeCreidelPueblo",n=18000,include_rts=F,lang="es") %>% 
@@ -25,11 +22,45 @@ test <- search_tweets("#TeCreidelPueblo",n=18000,include_rts=F,lang="es") %>%
   tibble() %>% 
   count(txt,sort=T)
 
-#Reset the dataset
-test <- test %>% mutate(best_fit=NA,similarity1=100,similarity2=1)
-test$replacement <- NULL
 
-#Stringdistance
+
+remove_duplicates <- function(vec,find_cutoff=FALSE,cutoff_distance=0.06) {
+  wordcount <- vec %>%
+    tibble(txt=vec) %>% 
+    dplyr::count(txt,sort=T)
+  
+  words <- wordcount$txt
+  
+  out <- sapply(seq_along(words)[-1],function(i) {
+    dist2 <- stringdist(words[i],words[1:i-1],method='jw',p=0.1)
+    best_fit <- which.min(dist2)
+    similarity2 <- min(dist2)
+    return(c(similarity2,best_fit))
+  }) %>% 
+    t() %>%
+    as.data.frame() %>% 
+    add_row(V1=1,V2=1,.before = 1) %>% 
+    cbind(wordcount) %>% 
+    dplyr::rename(distance=V1,best_fit=V2) %>% 
+    mutate(replacement=txt[best_fit])
+  
+  if(find_cutoff==TRUE) {return(out)}
+  
+  dict <- out %>% 
+    mutate(replacement=ifelse(distance<cutoff_distance,replacement,txt)) %>% 
+    filter(replacement!=txt)
+  
+  out_vec <- plyr::mapvalues(vec,from=dict$txt,to=dict$replacement)
+  
+  return(out_vec)
+}
+
+
+
+show <- remove_duplicates(test,find_cutoff=T)
+
+
+#Stringdistance with several distance measures
 
 lapply(seq_along(test$txt)[-1],function(i) {
   dist1 <- stringdist(test$txt[i],test$txt[1:i-1],method='lv')
